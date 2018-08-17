37219255f4793fd52566a8eb09976ed030320ed3
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-08-17 20:34:20 +0530

    Update and rename README.md to even.java

---
+++ even.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	System.out.println("the even number");
+	for(int i=1;i<100;i++)
+	if((i%2)==0)
+	{
+		System.out.println(i+"");
+	}
+	}
+}
